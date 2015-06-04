79f9cfba8d23f85667c869c5a4cbcad5a33777e2
Danai Adkisson <dadkisso@live.com>
2015-06-04 16:27:49 -0400

    fixed some tests, pushing to work seperate

--- test/controllers/weights_controller_test.rb
+++ test/controllers/weights_controller_test.rb
@@ -1,20 +1,47 @@
 require 'test_helper'
 
 class WeightsControllerTest < ActionController::TestCase
+
+  setup do
+    @weight = weights(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, id: weights(:one).id
     assert_response :success
   end
 
+
+
+
   test "should get create" do
-    get :create
+    post :create, weight: { weigh_date: @weight.weigh_date, weight: @weight.weight }
     assert_response :success
   end
+  #assert_redirected_to step_path(assigns(:step))
+
+
+
+  # test "should create step" do
+  #   assert_difference('Step.count') do
+  #     post :create, step: { step_date: @step.step_date, steps_taken: @step.steps_taken }
+  #   end
+  #
+  #   assert_redirected_to step_path(assigns(:step))
+  # end
+
+
+
+
+
+
+
+
 
 
   test "should get new" do
@@ -23,17 +50,17 @@ class WeightsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: weights(:one).id
     assert_response :success
   end
 
   test "should get update" do
-    get :update
+    get :update, id: weights(:one).id
     assert_response :success
   end
 
   test "should get destroy" do
-    get :destroy
+    get :destroy, id: weights(:one).id
     assert_response :success
   end
 
